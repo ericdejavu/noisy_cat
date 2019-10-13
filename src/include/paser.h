93392e1d5f663bb0c6cd3eae79c6e099ae628f18
eric <m17805802047@163.com>
2019-10-14 00:40:37 +0800

    [dev] test idea of chain

--- src/include/paser.h
+++ src/include/paser.h
@@ -1,6 +1,15 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#define DEFAULT_PATH_SIZE 1024
+
+#define DEFAULT_DEV_PATH "./router/dev.json"
+#define DEFAULT_MAJOR_PATH "./router/dev.json"
+
+#define DEFAULT_HASH_PATH "./router/auto/hash.json"
+#define DEFAULT_CHAIN_PATH "./router/auto/chain.json"
+#define DEFAULT_STABLE_PATH "./router/auto/stable.json"
+
 class Respnose {
 public:
 	char *status;
@@ -10,15 +19,37 @@ public:
 	~Respnose();
 };
 
+class ReturnEntity {
+public:
+	Respnose *respnose;
+	std::vector<char*> post_keys;
+};
+
+class JsonFile {
+public:
+	char *filepath;
+	int fd;
+
+	JsonFile();
+	~JsonFile();
+};
+
 class BaseParser {
 private:
-	bool parse_url_json(char *file_path);
-	bool parse_major_json(char *file_path);
 
+	JsonFile *dev_file, *hash_file, *chain_file, *major_file, *stable_file;
+
+	bool parse_url_json();
+	bool parse_major_json();
+
+	void update_chain(std::vector<std::string> post_keys);
 	void search_from_all_urls(char *uri);
 
 public:
-	virtual Respnose *dy_service(std::vector<std::string> post_datas);
+	BaseParser();
+	~BaseParser();
+	void setFilePath(char *dev_path, char *hash_path, char *chain_path, char *major_path, char *stable_path);
+	virtual ReturnEntity *dy_service(std::vector<std::string> post_datas);
 	void ev_handler(struct mg_connection *nc, int ev, void *ev_data);
 };
 
@@ -26,12 +57,12 @@ class PythonParser : public BaseParser {
 private:
 	Python *pPython;
 public:
-	Respnose *dy_service(std::vector<std::string> post_datas);
+	ReturnEntity *dy_service(std::vector<std::string> post_datas);
 };
 
 class JavaParser : public BaseParser {
 public:
-	Respnose *dy_service(std::vector<std::string> post_datas);
+	ReturnEntity *dy_service(std::vector<std::string> post_datas);
 };
 
 
--- src/parser.cpp
+++ src/parser.cpp
@@ -13,23 +13,74 @@ rapidjson::Document doc, hash_doc;
 #define STAND_LENGTH 4096
 
 #define COMMON_RESPONSE "{ \"ok\": %s, \"data\": %s }"
+#define DEFINE_NEW_FILE(file, path) 	file = new JsonFile(); \
+										file->filepath = new char[DEFAULT_PATH_SIZE]; \
+										memset(file->filepath, 0, sizeof(char)*DEFAULT_PATH_SIZE); \
+										memcpy(file->filepath, path, strlen(path));
 
 #define SEND(respnose) 		mg_printf_http_chunk(np->nc, COMMON_RESPONSE, respnose->status, respnose->result); \
-													mg_send_http_chunk(np->nc, "", 0);
+							mg_send_http_chunk(np->nc, "", 0);
 
-bool BaseParser::parse_url_json(char *hash_path, char *file_path) {
-	scan_dev_json(hash_path, file_path);
+
+
+JsonFile::JsonFile() : filepath(NULL), fd(0) {}
+
+JsonFile::~JsonFile() {
+	if (filepath != NULL) {
+		delete filepath;
+	}
+	if (fd != 0 && fd != -1) {
+		close(fd);
+	}
 }
 
-void BaseParser::scan_dev_json(char *hash_path, char *file_path) {
-	int dev_fd = open(file_path, O_RDONLY, 0665);
-	if (dev_fd == -1) {
+BaseParser::BaseParser() {
+	DEFINE_NEW_FILE(dev_file, DEFAULT_DEV_PATH);
+	DEFINE_NEW_FILE(major_file, DEFAULT_MAJOR_PATH);
+
+	DEFINE_NEW_FILE(hash_file, DEFAULT_HASH_PATH);
+	DEFINE_NEW_FILE(chain_file, DEFAULT_CHAIN_PATH);
+	DEFINE_NEW_FILE(stable_file, DEFAULT_STABLE_PATH);
+}
+
+BaseParser::~BaseParser() {
+	delete dev_file;
+	delete major_file;
+
+	delete hash_file;
+	delete chain_file;
+	delete stable_file;
+}
+
+bool BaseParser::parse_url_json() {
+	scan_dev_json(true);
+}
+
+
+/**
+write: i_arg1/u_arg2_arg1
+dev normal key: arg1
+hash_normal key: arg1_<index>
+chain normal key: <index>:arg1/<index>:arg2->arg1
+
+1. from dev file's url get post_datas
+2. analyze post_datas and write to hash file
+3. change dev file's post_datas to normal
+4. update chain file
+*/
+void BaseParser::scan_dev_json(bool closed_when_done) {
+	if (dev_file == NULL || hash_file == NULL) {
+		std::cout << "dev file and hash_file is null" << std::endl;
+		return;
+	}
+	dev_file->fd = open(dev_file->file_path, O_RDONLY, 0665);
+	if (dev_file->fd == -1) {
 		std::cout << "failed to load dev json file" << std::endl;
 		return false;
 	}
 
-	int hash_fd = open(file_path, O_CREAT | O_RDWR, 0665);
-	if (hash_fd == -1) {
+	hash_file->fd = open(hash_file->file_path, O_CREAT | O_RDWR, 0665);
+	if (hash_file->fd == -1) {
 		std::cout << "failed to load hash json file" << std::endl;
 		return false;
 	}
@@ -38,8 +89,8 @@ void BaseParser::scan_dev_json(char *hash_path, char *file_path) {
 	memset(dev_content, 0, sizeof(dev_content));
 	memset(hash_content, 0, sizeof(hash_content));
 
-	read(dev_fd, dev_content, MAX_CONTENT);
-	read(hash_fd, hash_content, MAX_CONTENT);
+	read(dev_file->fd, dev_content, MAX_CONTENT);
+	read(hash_file->fd, hash_content, MAX_CONTENT);
 
 	if (doc.Parse(dev_content).HasError()) {
 		return false;
@@ -63,15 +114,26 @@ void BaseParser::scan_dev_json(char *hash_path, char *file_path) {
 
 
 	}
-	close(dev_fd);
-	close(hash_fd);
+	if (closed_when_done) {
+		close(dev_file->fd);
+		dev_file->fd = 0;
+		close(hash_file->fd);
+		hash_file->fd = 0;
+	}
 
 	return true;
 }
 
-bool BaseParser::parse_major_json(char *file_path) {
-	int fd = open(file_path, O_RDONLY, 0665);
-	if (fd < 0) {
+void BaseParser::update_chain(std::vector<std::string> post_keys) {
+
+}
+
+bool BaseParser::parse_major_json() {
+	if (major_file == NULL) {
+		return false;
+	}
+	major_file->fd = open(major_file->file_path, O_RDONLY, 0665);
+	if (major_file->fd == -1) {
 		std::cout << "failed to load json file" << endl;
 		return false;
 	}
@@ -79,7 +141,7 @@ bool BaseParser::parse_major_json(char *file_path) {
 	char content[MAJOR_CONTENT];
 	memset(content, 0, sizeof(content));
 
-	read(fd, content, MAJOR_CONTENT);
+	read(major_file->fd, content, MAJOR_CONTENT);
 
 	if (doc.Parse(content).HasError()) {
 		return false;
@@ -94,13 +156,14 @@ bool BaseParser::parse_major_json(char *file_path) {
 
 		}
 	}
-	close(fd);
+	close(major_file->fd);
+	major_file->fd = 0;
 
 	return true;
 }
 
 
-Respnose *BaseParser::dy_service(std::vector<std::string> post_datas) {
+ReturnEntity *BaseParser::dy_service(std::vector<std::string> post_datas) {
 
 }
 
@@ -124,8 +187,12 @@ void BaseParser::search_from_all_urls(char *uri) {
 					}
 				}
 
-				Respnose *respnose = dy_service(post_datas);
-				SEND(respnose);
+				ReturnEntity *returnEntity = dy_service(post_datas);
+				if (returnEntity != NULL) {
+					update_chain(returnEntity->post_keys);
+					SEND(returnEntity->response);
+					delete returnEntity;
+				}
 			}
 			break;
 		}
